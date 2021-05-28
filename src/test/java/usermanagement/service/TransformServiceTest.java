ae661554ba858b13f88d59388c730e5097a95233
Sina <stavakkoli1999@gmail.com>
2021-05-28 23:59:51 +0430

    transform test added

--- src/test/java/usermanagement/service/TransformServiceTest.java
+++ src/test/java/usermanagement/service/TransformServiceTest.java
@@ -1,3 +1,36 @@
 package usermanagement.service;
 
-public class TransformServiceTest { }
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.Test;
+import usermanagement.entity.Person;
+
+
+public class TransformServiceTest { 
+private TransformService testClass = new TransformService();
+
+
+
+    @Test
+    public void test_toUserEntity() {
+        User usr = new User();
+        usr.setFirstName("fName");
+        usr.setLastName("lName");
+        usr.setCompanyName("companyName");
+        usr.setUserId(1);
+        Person testPer1 = testClass.toUserEntity(usr);
+
+        Person testPer2 = new Person();
+        testPer2.setfName(usr.getFirstName());
+        testPer2.setlName(usr.getLastName());
+        testPer2.setPersonId(usr.getUserId());
+        testPer2.setCompanyName(usr.getCompanyName());
+        assertEquals(testPer1.getfName(), testPer2.getfName());
+        assertEquals(testPer1.getlName(), testPer2.getlName());
+        assertEquals(testPer1.getCompanyName(), testPer2.getCompanyName());
+        assertEquals(testPer1.getPersonId(), testPer2.getPersonId());
+    }
+
+
+}
